570215b20caf11f7b72bb457e24b0882d92c7d56
Carlos Brito Lage <carlos@carloslage.net>
2012-04-02 00:44:23 +0100

    Initial working version of the cache :D

--- cachelicious.js
+++ cachelicious.js
@@ -2,10 +2,128 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 var strean = require('stream');
+var LRU = require("./vendor/node-lru-cache");
+var util = require("util");
+var events = require("events");
 
-Cachelicious = function (basepath, port)
+function CacheStream(size) {
+	events.EventEmitter.call(this);
+	this.buffer = new Buffer(size);
+	this.size = size;
+	this.writeOffset = 0;
+	this.activeRequests = {};
+	this.activeRequestCount = 0;
+	this.requestServer = null;
+	this.endendWriting = false;
+	console.log('initialized buffer with size:' + this.size);
+}
+
+util.inherits(CacheStream, events.EventEmitter);
+
+CacheStream.prototype.register = function (requestId) 
 {
-	this.init(basepath, port)
+	this.activeRequests[requestId] = {
+		paused: false,
+		readOffset: 0,
+	};
+	console.log("registering requestId:" + requestId);
+	
+	if (0 === this.activeRequestCount++) {
+		this.startServingRequests();
+	}
+
+}
+
+CacheStream.prototype.startServingRequests = function () 
+{
+	var self = this;
+	this.requestServer = setInterval(function () {
+		if (0 ===	self.activeRequestCount) {
+			clearInterval(self.requestServer);
+			return;
+		}
+		
+		for (var requestId in self.activeRequests) {
+			if (self.activeRequests.hasOwnProperty(requestId)) {
+				self.emitRequestData(requestId);
+			}
+		}
+	}, 0);
+	
+}
+
+CacheStream.prototype.emitRequestData = function (requestId) 
+{
+	if (this.activeRequests[requestId].paused || this.activeRequests[requestId].readOffset === this.writeOffset) {
+		return;
+	}
+	console.log("emitting data for requestId:" + requestId);
+	var self = this;
+	setTimeout(function () {		
+		if (undefined === self.activeRequests[requestId]) {
+			console.log("abandoning streaming for req that ended before this setimeout:" + requestId);
+			return;
+		}
+		self.emit(
+			requestId+"data", 
+			self.buffer.slice(self.activeRequests[requestId].readOffset, self.writeOffset)
+		);
+		
+		self.activeRequests[requestId].readOffset = self.writeOffset;
+		if (self.activeRequests[requestId].readOffset === self.size) {
+			console.log("ending streaming for req:" + requestId);
+			self.emit(requestId+"end");
+			self.activeRequestCount--;
+			delete self.activeRequests[requestId];
+		}
+	}, 0);	
+}
+
+CacheStream.prototype.pause = function (requestId) 
+{
+	this.activeRequests[requestId].paused = true;
+}
+
+CacheStream.prototype.resume = function (requestId) 
+{
+	this.activeRequests[requestId].paused = false;
+}
+
+CacheStream.prototype.write = function(data) 
+{
+	if (this.endendWriting) {
+		console.log("rejecting write because we ended already")
+		return;
+	}
+	
+	var writtenBytes;
+	if (Buffer.isBuffer(data)) {
+		console.log("writing buffer data " + data.length + " bytes to offset: " + this.writeOffset);
+		data.copy(this.buffer, this.writeOffset);
+		writtenBytes = data.length;
+	} else {
+		console.log("writing " + Buffer.byteLength(data) + " bytes to offset: " + this.writeOffset);
+		writtenBytes = this.buffer.write(data, this.writeOffset);
+	}
+	this.writeOffset+= writtenBytes;
+	console.log("wrote " + writtenBytes + " bytes into buffer");
+	console.log("new offset" + this.writeOffset);		
+}
+
+CacheStream.prototype.end = function() 
+{
+	console.log("ended!");
+	this.endendWriting = true;
+}
+
+
+
+
+
+
+Cachelicious = function (basepath, port, maxCacheSize)
+{
+	this.init(basepath, port, maxCacheSize)
 };
 
 Cachelicious.prototype = {
@@ -20,8 +138,21 @@ Cachelicious.prototype = {
 			port = 9876;
 		}
 		
+		if (typeof maxCacheSize === 'undefined') {
+			maxCacheSize = 20971520; //20mb
+		}
+		
+		if (maxCacheSize !== false) {
+			this.cache = LRU(maxCacheSize, function (cacheStream) {
+				return cacheStream.size;
+			});			
+		} else {
+			this.cache = false;
+		}
+		
 		this.port = port;
 		this.basepath = basepath;
+		this.requestStreamId = 0;
 		
 		this.server = http.createServer(function (request, response) {
 			self.dispatch(request, response);
@@ -69,6 +200,8 @@ Cachelicious.prototype = {
 				return;				
 			}			
 			
+			//console.log(stat);
+			
 			self.servePath(filepath, stat.size, response);
 		});
 	}, 
@@ -98,26 +231,34 @@ Cachelicious.prototype = {
 			'Content-Type':   contentType,
 			'Content-Length': size
 		});
-		this.streamPath(filepath, response);		
+		this.streamPath(filepath, size, response);
 	},
 	
-	streamPath: function (filepath, response)
+	streamPath: function (filepath, size, response)
 	{
-		var readStream = fs.createReadStream(filepath);
-		readStream.on('data', function(data) {
+		this.requestStreamId++;
+		
+		var readStream = this.getReadStream(filepath, size), requestId = "r-" + this.requestStreamId;
+
+
+		readStream.register(requestId);
+		
+		readStream.on(requestId+'data', function(data) {
+			console.log(requestId + "Streaming data back to client")
+			
 			var flushed = response.write(data);
 			// Pause the read stream when the write stream gets saturated
 			if(!flushed) {
-				readStream.pause();	
+				readStream.pause(requestId);	
 			}
 		});
 
 		response.on('drain', function() {
 			// Resume the read stream when the write stream gets hungry 
-			readStream.resume();
+			readStream.resume(requestId);
 		});
 
-		readStream.on('end', function() {
+		readStream.on(requestId+'end', function() {
 			response.end();
 		});
 	},
@@ -126,8 +267,48 @@ Cachelicious.prototype = {
 	{
 		console.log('error:' + code);
 		response.writeHead(code, { 'Content-Type': 'text/html' });
-		response.end(""+code, 'utf-8');;		
-	}
+		response.end(""+code, 'utf-8');		
+	},
+	
+	getReadStream: function (filepath, size) 
+	{
+		var cacheStream = this.getCached(filepath), self = this;
+		if (false === cacheStream || undefined === cacheStream) {
+			cacheStream = new CacheStream(size);
+			this.setCached(filepath, cacheStream);
+			
+			var fileReadSteam = fs.createReadStream(filepath);
+
+			fileReadSteam.on('data', function (data) {
+				cacheStream.write(data);
+			});
+			
+			fileReadSteam.on('end', function() {
+				cacheStream.end();
+			});
+			
+		}
+		
+		return cacheStream;
+	},
+	
+	getCached: function (filepath) 
+	{
+		if (this.cache === false) {
+			return false;
+		}
+		return this.cache.get(filepath);
+	},
+	
+	setCached: function (filepath, cacheStream) 
+	{
+		if (this.cache === false) {
+			return;
+		}
+		this.cache.set(filepath, cacheStream);
+	},
+	
+
 
 
 };
